986d0792379bf5f5974997ec8a923e632b56e668
unknown <sarthakagrawal927@gmail.com>
2020-12-23 18:09:36 +0530

    d

---
+++ EndsemPrep/CD/q1out.c
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+main()
+{
+    int a;
+    long b;      
+    long long c; 
+    double e;
+    long double f;
+    
+    printf("Size of int = %zu bytes \n", sizeof(a));
+    return 0;
+}
